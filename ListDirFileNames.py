22e255471a3437afade7d17078a0cf68ab2d5809
homerflander <van.vincent2013@gmail.com>
2017-06-21 01:30:55 -0700

    V0.02 Fixed Reading in self

--- ListDirFileNames.py
+++ ListDirFileNames.py
@@ -5,10 +5,19 @@ if len(sys.argv) is not 2:#takes in one argument for output textfile.
     sys.exit("Error, script needs one command-line argument. (output.txt File)")
 
 list=""
+start=0
+end=len(sys.argv[0])
+#print(start)
+#print(end)
+
+while(sys.argv[0].find("/",start,end)!=-1):
+    #print(sys.argv[0].find("/",start,end))
+    start=sys.argv[0].find("/",start,end)+1
 
 dirList = os.listdir("./")
 for filename in dirList:
-    list=list+filename+"\n"
+    if(filename!=sys.argv[0][start:end]):
+        list=list+filename+"\n"
 
 print("Now writing the following list of file names to %s" %sys.argv[1])
 print(list)
